43ee247afe34a1330bb843af4b1335c125dc0c0a
Codestar007 <kjeged01@dcs.bbk.ac.uk>
2018-04-30 19:17:48 +0100

    Q2  answered

--- Q2.py
+++ Q2.py
@@ -2,11 +2,19 @@ import random
 
 total = 0
 randNum = random.randrange(0,100)
+print(randNum)
 response = -1
 prompt = "Guess a number between 0 and 99: "
 while response != randNum:
     total += 1
     response = int(input(prompt))
-    if response != randNum:
+    if response == randNum:
         print("Correct. It took you", total, "guesses.")
-        break
+        #break
+    elif response > randNum:
+        prompt = "Too high. Guess again: "
+        #print("Too high. Guess again: ")
+    else:
+        prompt = "Too low. Guess again: "
+        #print("Too low. Guess again: ")
+
